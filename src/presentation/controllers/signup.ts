46422b665868a97fba2d9f7790a1d509bbfa1d2d
Henrique Campaner <henrique.campaner@live.com>
2020-12-29 20:29:58 +0000

    feat: ensure SignUpController returns 400 if passwordconfirmation is differente

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -12,15 +12,17 @@ export class SignUpController implements Controller {
   handle (httpRequest: HttpRequest): HttpResponse {
     try {
       const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
-
       for (const field of requiredFields) {
         if (!httpRequest.body[field]) {
           return badRequest(new MissingParamError(field))
         }
       }
 
-      const isValid = this.emailValidator.isValid(httpRequest.body.email)
+      if (httpRequest.body.password !== httpRequest.body.passwordConfirmation) {
+        return badRequest(new InvalidParamError('passwordConfirmation'))
+      }
 
+      const isValid = this.emailValidator.isValid(httpRequest.body.email)
       if (!isValid) {
         return badRequest(new InvalidParamError('email'))
       }
