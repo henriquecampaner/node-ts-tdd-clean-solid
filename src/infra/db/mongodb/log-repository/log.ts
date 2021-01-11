a92a158a3a48ec7dc11a3bcb0e78b2ada64499cc
Henrique Campaner <henrique.campaner@live.com>
2021-01-11 20:29:43 +0000

    feat: ensure ExpressRoutesAdapter returns error message instead of the error name

--- src/infra/db/mongodb/log-repository/log.ts
+++ src/infra/db/mongodb/log-repository/log.ts
@@ -1,7 +1,7 @@
 import { LogErrorRepository } from '../../../../data/protocols/log-error-repository'
 import { MongoHelper } from '../helpers/mongo-helper'
 
-export class LogoMongoRepository implements LogErrorRepository {
+export class LogMongoRepository implements LogErrorRepository {
   async logError (stack: string): Promise<void> {
     const errorCollection = await MongoHelper.getCollection('errors')
     await errorCollection.insertOne({
