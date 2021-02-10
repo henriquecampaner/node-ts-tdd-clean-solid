38b3bbe8f1dabf3559aba03a4cd97df77ce503ee
Henrique Campaner <henrique.campaner@live.com>
2021-02-10 18:03:32 +0000

    test: ensure RequiredFieldValidation do not return if validation succeeds

--- src/presentation/helpers/validators/required-field-validation.spec.ts
+++ src/presentation/helpers/validators/required-field-validation.spec.ts
@@ -10,4 +10,13 @@ describe('RequiredField Validation', () => {
     })
     expect(error).toEqual(new MissingParamError('field'))
   })
+
+  it('should not return if validation succeeds', () => {
+    const sut = new RequiredFieldValidation('field')
+
+    const error = sut.validate({
+      field: 'any_name'
+    })
+    expect(error).toBeFalsy()
+  })
 })
