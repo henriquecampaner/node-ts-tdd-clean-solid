6bacd6e54ec3a374af22ccf56de2c34918c74491
Henrique Campaner <henrique.campaner@live.com>
2021-02-11 20:25:56 +0000

    feat: ensure BCryptAdapter returns true when compare succeeds

--- src/infra/criptography/bcrypt-adapter.spec.ts
+++ src/infra/criptography/bcrypt-adapter.spec.ts
@@ -49,4 +49,12 @@ describe('Bcrypt Adapter', () => {
 
     expect(compareSpy).toHaveBeenCalledWith('any_value', 'any_hash')
   })
+
+  test('Should return a true when compare succeeds', async () => {
+    const sut = makeSut()
+
+    const isValid = await sut.compare('any_value', 'any_isValid')
+
+    expect(isValid).toBe(true)
+  })
 })
