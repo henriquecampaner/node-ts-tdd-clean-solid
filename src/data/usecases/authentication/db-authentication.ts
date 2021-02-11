8c1a12fb3b505a6afcd1db895471218d3160d221
Henrique Campaner <henrique.campaner@live.com>
2021-02-11 19:23:30 +0000

    feat: ensure DbAuthentication returns token on success

--- src/data/usecases/authentication/db-authentication.ts
+++ src/data/usecases/authentication/db-authentication.ts
@@ -21,8 +21,12 @@ export class DbAuthentication implements Authentication {
     const account = await this.loadAccountByEmailRepository.load(authentication.email)
 
     if (account) {
-      await this.hashCompare.compare(authentication.password, account.password)
-      await this.tokenGenerator.generate(account.id)
+      const isValid = await this.hashCompare.compare(authentication.password, account.password)
+
+      if (isValid) {
+        const accessToken = await this.tokenGenerator.generate(account.id)
+        return accessToken
+      }
     }
 
     return null
