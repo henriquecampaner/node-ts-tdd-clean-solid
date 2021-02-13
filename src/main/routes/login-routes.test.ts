6dc41702b0ce6dcc7e068c905de52ddd7956418c
Henrique Campaner <henrique.campaner@live.com>
2021-02-13 19:43:48 +0000

    refactor: rename signup routes to login routes

--- src/main/routes/login-routes.test.ts
+++ src/main/routes/login-routes.test.ts
@@ -28,3 +28,30 @@ describe('Signup Routes', () => {
       .expect(200)
   })
 })
+
+describe('POST / signup', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    const accountCollection = await MongoHelper.getCollection('accounts')
+    await accountCollection.deleteMany({})
+  })
+
+  test('Should return 200 on signup', async () => {
+    await request(app)
+      .post('/api/signup')
+      .send({
+        name: 'Henrique',
+        email: 'henrique@campaner.com',
+        password: 'password123',
+        passwordConfirmation: 'password123'
+      })
+      .expect(200)
+  })
+})
