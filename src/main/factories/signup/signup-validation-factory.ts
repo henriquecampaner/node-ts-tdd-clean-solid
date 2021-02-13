ca56f59087de89f65366567451e756f811fa8bf5
Henrique Campaner <henrique.campaner@live.com>
2021-02-13 19:39:34 +0000

    refactor: move emailValidator to main layter

--- src/main/factories/signup/signup-validation-factory.ts
+++ src/main/factories/signup/signup-validation-factory.ts
@@ -1,6 +1,6 @@
 import { ComparefieldsValidation, EmailValidation, RequiredFieldValidation, ValidationComposit } from '../../../presentation/helpers/validators'
 import { Validation } from '../../../presentation/protocols/validation'
-import { EmailValidatorAdapter } from '../../../utils/email-validator-adapter'
+import { EmailValidatorAdapter } from '../../adapters/validators/email-validator-adapter'
 
 export const makeSignupValidation = ():ValidationComposit => {
   const validations: Validation[] = []
