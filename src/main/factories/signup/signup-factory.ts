3dfd8a2633ce91956ad9c8874a75ce7d97b69347
Henrique Campaner <henrique.campaner@live.com>
2021-02-16 13:48:35 +0000

    fix: asignup controller

--- src/main/factories/signup/signup-factory.ts
+++ src/main/factories/signup/signup-factory.ts
@@ -6,15 +6,26 @@ import { SignUpController } from '../../../presentation/controllers/signup/signu
 import { Controller } from '../../../presentation/protocols'
 import { LogControllerDecorator } from '../../decorators/log-controller-decorator'
 import { makeSignupValidation } from './signup-validation-factory'
+import { DbAuthentication } from '../../../data/usecases/authentication/db-authentication'
+import { JwtAdapter } from '../../../infra/criptography/jwt/jwt-adapter'
+import env from '../../config/env'
 
 export const makeSignUpController = ():Controller => {
   const accountMongoRepository = new AccountMongoRepository()
   const salt = 12
   const hasher = new BcryptAdapter(salt)
+  const jwtAdapter = new JwtAdapter(env.jwtSecret)
+
+  const dbAuthentication = new DbAuthentication(
+    accountMongoRepository,
+    hasher,
+    jwtAdapter,
+    accountMongoRepository
+  )
 
   const addAccount = new DbAddAccount(hasher, accountMongoRepository)
   const signUpController = new SignUpController(
-    addAccount, makeSignupValidation())
+    addAccount, makeSignupValidation(), dbAuthentication)
 
   const logMongoRepository = new LogMongoRepository()
 
