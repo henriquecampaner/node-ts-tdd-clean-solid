6e4fcaeb3abeed14f7bd42e63f4a3f25a6081519
Henrique Campaner <henrique.campaner@live.com>
2021-02-17 20:47:59 +0000

    feat: ensure AddSurveyController return 204 on success

--- src/presentation/controllers/survey/add-survey/add-survey-controller.spec.ts
+++ src/presentation/controllers/survey/add-survey/add-survey-controller.spec.ts
@@ -1,6 +1,6 @@
 import { HttpRequest, AddSurvey, AddSurveyModel, Validation } from './add-survey-controller-protocols'
 import { AddSurveyController } from './add-survey-controller'
-import { badRequest, serverError } from '../../../helpers/http/http-helper'
+import { badRequest, serverError, noContent } from '../../../helpers/http/http-helper'
 
 const makeValidationStub = ():Validation => {
   class ValidationStub implements Validation {
@@ -97,5 +97,13 @@ describe('AddSurvey Controller', () => {
     const httpResponse = await sut.handle(makeFakeRequest())
 
     expect(httpResponse).toEqual(serverError(new Error()))
+  })
+
+  it('should returns 204 on success', async () => {
+    const { sut } = makeSut()
+
+    const httpResponse = await sut.handle(makeFakeRequest())
+
+    expect(httpResponse).toEqual(noContent())
   })
 })
