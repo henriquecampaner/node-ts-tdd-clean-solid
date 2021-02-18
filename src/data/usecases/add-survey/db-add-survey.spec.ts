85e81e6546e23b05d3d4feaa032a81e1f7326ed7
Henrique Campaner <henrique.campaner@live.com>
2021-02-18 19:03:32 +0000

    test: ensure AddSurvey calls addSurveyRepository with correct values

---
+++ src/data/usecases/add-survey/db-add-survey.spec.ts
@@ -0,0 +1,44 @@
+import { AddSurveyModel, AddSurveyRepository } from './db-add-survey-protocols'
+import { DbAddSurvey } from './db-add-survey'
+
+const makeFakeSurveyData = ():AddSurveyModel => ({
+  question: 'any_question',
+  answers: [
+    {
+      image: 'any_image',
+      answer: 'any_answer'
+    }
+  ]
+})
+
+const makeAddSurveyRepositoryStub = ():AddSurveyRepository => {
+  class AddSurveyRepositoryStub implements AddSurveyRepository {
+    add (surveyData: AddSurveyModel): Promise<void> {
+      return new Promise(resolve => resolve())
+    }
+  }
+
+  return new AddSurveyRepositoryStub()
+}
+
+const makeSut = () => {
+  const addSurveyRepositoryStub = makeAddSurveyRepositoryStub()
+  const sut = new DbAddSurvey(addSurveyRepositoryStub)
+
+  return {
+    addSurveyRepositoryStub,
+    sut
+  }
+}
+
+describe('DbAddSurvey UseCase', () => {
+  it('should call AddSurveyRepository with correct values', async () => {
+    const { sut, addSurveyRepositoryStub } = makeSut()
+
+    const addSpy = jest.spyOn(addSurveyRepositoryStub, 'add')
+
+    await sut.add(makeFakeSurveyData())
+
+    expect(addSpy).toHaveBeenCalledWith(makeFakeSurveyData())
+  })
+})
