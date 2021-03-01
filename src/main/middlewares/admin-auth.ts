3263e38d06df4c9ceb63acedc0af9cd6fe34899e
Henrique Campaner <henrique.campaner@live.com>
2021-03-01 20:14:02 +0000

    refactor: move middleware authentication

---
+++ src/main/middlewares/admin-auth.ts
@@ -0,0 +1,4 @@
+import { adapterMiddleware } from '../adapters/express/express-middleware-adapter'
+import { makeAuthMiddleware } from '../factories/middlewares/auth-middleware-factory'
+
+export const adminAuth = adapterMiddleware(makeAuthMiddleware('admin'))
--- src/main/routes/survey-routes.ts
+++ src/main/routes/survey-routes.ts
@@ -1,14 +1,11 @@
 import { Router } from 'express'
 import { adaptRoute } from '../adapters/express/express-route-adapter'
-import { adapterMiddleware } from '../adapters/express/express-middleware-adapter'
 import { makeAddSurveyController } from '../factories/controllers/survey/addSurvey/add-survey-controller-factory'
-import { makeAuthMiddleware } from '../factories/middlewares/auth-middleware-factory'
 import { makeLoadSurveyController } from '../factories/controllers/survey/loadSurveys/load-surveys-controller-factory'
+import { adminAuth } from '../middlewares/admin-auth'
+import { auth } from '../middlewares/auth'
 
 export default (router: Router): void => {
-  const adminAuth = adapterMiddleware(makeAuthMiddleware('admin'))
-  const auth = adapterMiddleware(makeAuthMiddleware())
-
   router.post('/surveys', adminAuth, adaptRoute(makeAddSurveyController()))
   router.get('/surveys', auth, adaptRoute(makeLoadSurveyController()))
 }
