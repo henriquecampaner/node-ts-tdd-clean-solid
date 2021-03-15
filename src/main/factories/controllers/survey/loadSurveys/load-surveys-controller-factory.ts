31f254947fbe6a9f9035e64f42a46c93c700de43
Henrique Campaner <henrique.campaner@live.com>
2021-03-15 15:09:10 +0000

    feat: ensure save survey result route returns 403 if no toke is provided

--- src/main/factories/controllers/survey/loadSurveys/load-surveys-controller-factory.ts
+++ src/main/factories/controllers/survey/loadSurveys/load-surveys-controller-factory.ts
@@ -2,7 +2,7 @@ import { Controller } from '@/presentation/protocols'
 import { LoadSurveysController } from '@/presentation/controllers/survey/load-surveys/load-surveys-controller'
 
 import { makeLogDecorator } from '@/main/factories/decorators/log-controller-decorator-factory'
-import { makeDbLoadSurveys } from '@/main/factories/usecases/survey/load-surveys/db-load-surveys'
+import { makeDbLoadSurveys } from '@/main/factories/usecases/survey/load-surveys/db-load-surveys-factory'
 
 export const makeLoadSurveyController = (): Controller => {
   const controller = new LoadSurveysController(makeDbLoadSurveys())
