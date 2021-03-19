9d99af02b0e34d3d2ccac53126e5f885d1b16ce7
Henrique Campaner <henrique.campaner@live.com>
2021-03-19 16:51:01 +0000

    feat: dbLoadSurveyResult calls repo if repo returns null

--- src/data/usecases/survey-result/load-survey-result/load-survey-result-protocols.ts
+++ src/data/usecases/survey-result/load-survey-result/load-survey-result-protocols.ts
@@ -1,3 +1,4 @@
 export * from '@/data/protocols/db/surveyResult/load-survey-result-repository'
 export * from '@/domain/models/survey-result'
 export * from '@/domain/useCases/survey-result/load-survey-result'
+export * from '@/domain/useCases/survey/load-survey-by-id'
--- src/data/usecases/survey-result/load-survey-result/load-survey-result.ts
+++ src/data/usecases/survey-result/load-survey-result/load-survey-result.ts
@@ -2,13 +2,23 @@ import {
   SurveyResultModel,
   LoadsurveyResult,
   LoadSurveyRepository,
+  LoadSurveyById,
 } from './load-survey-result-protocols'
 export class DbLoadSurveyResult implements LoadsurveyResult {
   constructor(
     private readonly loadSurveyResultRepository: LoadSurveyRepository,
+    private readonly loadSurveyById: LoadSurveyById,
   ) {}
 
   async load(surveyId: string): Promise<SurveyResultModel> {
-    return await this.loadSurveyResultRepository.loadBySurveyId(surveyId)
+    const surveyResult = await this.loadSurveyResultRepository.loadBySurveyId(
+      surveyId,
+    )
+
+    if (!surveyResult) {
+      await this.loadSurveyById.loadById(surveyId)
+    }
+
+    return surveyResult
   }
 }
