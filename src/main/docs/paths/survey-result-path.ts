2df2793acb5260abfae1e8bcfeb2a2432ef7b712
Henrique Campaner <henrique.campaner@live.com>
2021-03-22 17:44:30 +0000

    docs: add load survey result

--- src/main/docs/paths/survey-result-path.ts
+++ src/main/docs/paths/survey-result-path.ts
@@ -48,4 +48,45 @@ export const surveyResultPath = {
       },
     },
   },
+
+  get: {
+    security: [
+      {
+        apiKeyAuth: [],
+      },
+    ],
+    tags: ['Surveys'],
+    summary: 'Route to get the survey',
+    parameters: [
+      {
+        in: 'path',
+        name: 'surveyId',
+        required: true,
+        schema: {
+          type: 'string',
+        },
+      },
+    ],
+    responses: {
+      200: {
+        description: 'Success',
+        content: {
+          'application/json': {
+            schema: {
+              $ref: '#/schemas/surveyResult',
+            },
+          },
+        },
+      },
+      403: {
+        $ref: '#/components/forbidden',
+      },
+      500: {
+        $ref: '#/components/serverError',
+      },
+      404: {
+        $ref: '#/components/notFound',
+      },
+    },
+  },
 }
