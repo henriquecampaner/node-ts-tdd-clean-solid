bb97543f160c34a8ae92b8ca3f33c888c462daa9
Henrique Campaner <henrique.campaner@live.com>
2021-03-22 17:51:42 +0000

    refactor: remove unused code

--- src/infra/db/mongodb/helpers/queryBuilder.ts
+++ src/infra/db/mongodb/helpers/queryBuilder.ts
@@ -40,13 +40,6 @@ export class QueryBuilder {
     return this
   }
 
-  addFields(data: object): QueryBuilder {
-    this.query.push({
-      $addFields: data,
-    })
-    return this
-  }
-
   project(data: object): QueryBuilder {
     this.query.push({
       $project: data,
